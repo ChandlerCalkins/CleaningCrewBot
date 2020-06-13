5f216eb76f908dc82483597a0fcb3b730f14c605
ChaseDucc <49660926+ChaseDucc@users.noreply.github.com>
2020-06-12 20:30:55 -0700

    Added more redacts and !silence + !free commands
    Now redacts more things, changes everything to lowercase to check all cases of redacted words, and added the !silence and !free command which when @ing someone with silence redacts everything they say, and the free command allows them to message again.

--- bot.py
+++ bot.py
@@ -1,32 +1,59 @@
 import discord
 
 token = ""
+prefix = '!'
 client = discord.Client()
+silenced = 0
 
 @client.event
 async def on_message(message):
+    global silenced
     words = message.content.split()
 
-    x = 0
-    while x < len(words):
-        if words[x] == "uwu" or words[x] == "UwU" or words[x] == "uWu" or words[x] == "UWU":
+    index = 0
+    while index < len(words):
+        #redacts message from person who had silence command used on them
+        if message.author.id == silenced:
             await message.delete()
             await message.channel.send("[REDACTED]")
-        elif words[x] == "owo" or words[x] == "OwO" or words[x] == "oWo" or words[x] == "OWO":
+        #redacts uwu
+        elif words[index].lower() == "uwu":
             await message.delete()
             await message.channel.send("[REDACTED]")
-        elif words[x] == ":3":
+        #redacts owo
+        elif words[index].lower() == "owo":
             await message.delete()
             await message.channel.send("[REDACTED]")
-        elif words[x] == "furry":
+        #redacts cat face
+        elif words[index] == ":3":
+            await message.delete()
+            await message.channel.send("[REDACTED]")
+        #redacts the word furry
+        elif words[index].lower() == "furry":
             await message.delte()
             await message.channel.send("[REDACTED]")
-        elif words[x] == "technoblade" or words[x] == "Technoblade":
+        #redacts shy fingers
+        elif index < len(words) - 1 and words[index] == "👉" and words[index + 1] == "👈":
             await message.delete()
             await message.channel.send("[REDACTED]")
-        elif x < len(words) - 1 and words[x] == "👉" and words[x + 1] == "👈":
+        #redacts shy fingers around another emoji
+        elif index < len(words) - 2 and words[index] == "👉" and words[index + 2] == "👈":
             await message.delete()
             await message.channel.send("[REDACTED]")
-        x = x + 1
+        #redacts tongue emoji
+        elif words[index] == "👅":
+            await message.delete()
+            await message.channel.send("[REDACTED]")
+        #allows people to redact other people
+        elif len(words) == 2 and index == 0 and words[index] == prefix + "silence":
+            if message.author.id == 224746044502704130 or message.author.id == 215296706131001345 or message.author.id == 161216398159380480:
+                silenced = message.mentions[0].id
+                await message.channel.send("Silencing Initiated...")
+        #unsilences the silenced
+        elif len(words) == 1 and index ==0 and words[index] == prefix + "free":
+            if message.author.id == 224746044502704130 or message.author.id == 215296706131001345 or message.author.id == 161216398159380480:
+                silenced = 0
+                await message.channel.send("Silencing Terminated...")
+        index = index + 1
 
 client.run(token)
