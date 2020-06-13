8d0ab5581f45a9c0eb2eefe5e7a60213c53e5f28
ChaseDucc <49660926+ChaseDucc@users.noreply.github.com>
2020-06-12 18:19:30 -0700

    Redacts more, cleaned up
    Now redacts ":3", "furry", and variations of "technoblade"

--- bot.py
+++ bot.py
@@ -1,18 +1,29 @@
 import discord
 
-token = ""
+token = "NzIwMzg3ODA1NTM2MDU5NDE0.XuHL9A.AVvpTWGLJsHsmB4QFTnkzByXpVI"
 client = discord.Client()
 
 print("test")
 
 @client.event
 async def on_message(message):
-    print(message.content)
     words = message.content.split()
-    
-    x = 0;
+
+    x = 0
     while x < len(words):
-        if words[x] == "uwu" or words[x] == "UwU" or words[x] == "uWu" or words[x] == "UWU" or words[x] == "owo" or words[x] == "OwO" or words[x] == "oWo" or words[x] == "OWO":
+        if words[x] == "uwu" or words[x] == "UwU" or words[x] == "uWu" or words[x] == "UWU":
+            await message.delete()
+            await message.channel.send("[REDACTED]")
+        elif words[x] == "owo" or words[x] == "OwO" or words[x] == "oWo" or words[x] == "OWO":
+            await message.delete()
+            await message.channel.send("[REDACTED]")
+        elif words[x] == ":3":
+            await message.delete()
+            await message.channel.send("[REDACTED]")
+        elif words[x] == "furry":
+            await message.delte()
+            await message.channel.send("[REDACTED]")
+        elif words[x] == "technoblade" or words[x] == "Technoblade":
             await message.delete()
             await message.channel.send("[REDACTED]")
         elif x < len(words) - 1 and words[x] == "👉" and words[x + 1] == "👈":
