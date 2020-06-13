84c93b5a478ea45a4f1e59f383d450d22ef39407
ChaseDucc <49660926+ChaseDucc@users.noreply.github.com>
2020-06-12 17:38:31 -0700

    Now redacts shy finger pointing
    Bot now detects and redacts :point_right: and :point_left: emojis when they're next to each other in that order. Changed the loop through the message to go from a for loop to a while loop to make this work.

--- bot.py
+++ bot.py
@@ -9,9 +9,15 @@ print("test")
 async def on_message(message):
     print(message.content)
     words = message.content.split()
-    for x in words:
-        if x == "uwu" or x == "UwU" or x == "uWu" or x == "UWU" or x == "owo" or x == "OwO" or x == "oWo" or x == "OWO":
+    
+    x = 0;
+    while x < len(words):
+        if words[x] == "uwu" or words[x] == "UwU" or words[x] == "uWu" or words[x] == "UWU" or words[x] == "owo" or words[x] == "OwO" or words[x] == "oWo" or words[x] == "OWO":
             await message.delete()
             await message.channel.send("[REDACTED]")
+        elif x < len(words) - 1 and words[x] == "👉" and words[x + 1] == "👈":
+            await message.delete()
+            await message.channel.send("[REDACTED]")
+        x = x + 1
 
 client.run(token)
